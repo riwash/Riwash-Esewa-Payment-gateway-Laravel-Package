b60bcc2fc9d2360de737dcbd226db5e456a945cd
unknown <technicalriwash@gmail.com>
2024-02-09 12:31:44 +0545

    upload esewa v2 payment gateway

--- src/RiwashEsewa.php
+++ src/RiwashEsewa.php
@@ -1,17 +1,20 @@
 <?php
 namespace Riwash\Esewa;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Config;
 
 class RiwashEsewa
 {
     private $merchant_id;
     private $accessType;
+    private $proCode;
 
     public function __construct()
     {
         $this->merchant_id = config('riwashesewa.secret_key');
         $this->accessType = config('riwashesewa.access_type');
+        $this->proCode = config('riwashesewa.prod_code');
     }
 
     public function esewaCheckout($data)
@@ -25,22 +28,47 @@ class RiwashEsewa
         }
 
         if (!$this->merchant_id) {
+
             throw new \Exception("Please Enter Merchant Id");
         }
 
-        $data['tAmt'] = $data['amt'] + $data['pdc'] + $data['psc'] + $data['txAmt'];
-
-        $data['scd'] = $this->merchant_id;
-
         try {
 
-            $url = $esewa_url . http_build_query($data);
+            $amount = $data['total_amount'];
+            $transaction_uuid = $data['transaction_uuid'];
+            $message = "total_amount=$amount,transaction_uuid=$transaction_uuid,product_code=$this->proCode";
+            $s = hash_hmac('sha256', $message, config('riwashesewa.secret_key'), true);
+            $signature = base64_encode($s);
+            $data = [
+                "amount" => $data['amount'],
+                "failure_url" => route('esewa.fail'),
+                "product_delivery_charge" => $data['product_delivery_charge'],
+                "product_service_charge" => $data['product_service_charge'],
+                "product_code" => "EPAYTEST",
+                "signed_field_names" => "total_amount,transaction_uuid,product_code",
+                "success_url" => route('esewa.success'),
+                "tax_amount" => $data['tax_amount'],
+                "total_amount" => $data['total_amount'],
+                "transaction_uuid" => $data['transaction_uuid'],
+                "signature" => $signature,
 
-            return $url;
+            ];
+            $sendUrl = $esewa_url . http_build_query($data);
+            $response = Http::post($sendUrl);
+
+            if ($response->successful()) {
+                $redirectUrl = $response->handlerStats()['url'];
+
+                return redirect()->away($redirectUrl);
+
+            } else {
+
+                return $response->body();
+            }
 
         } catch (\Exception $e) {
 
-            throw new \Exception('Oops something is happen !');
+            return $e;
         }
 
     }
--- src/config/riwashesewa.php
+++ src/config/riwashesewa.php
@@ -4,7 +4,8 @@
 return [
     'access_type' => env('ESEWA_ACCESS_TYPE', 'Test'),
     'website_url' => env('WEBSITE_URL'),
-    'secret_key' => env('ESEWA_MERCHANT_ID', 'EPAYTEST'),
-    'test_esewa_url' => 'https://uat.esewa.com.np/epay/main/?',
+    'secret_key' => env('ESEWA_MERCHANT_ID', '8gBm/:&EnhH.1/q'),
+    'prod_code' => env('ESEWA_PROD_CODE', 'EPAYTEST'),
+    'test_esewa_url' => 'https://rc-epay.esewa.com.np/api/epay/main/v2/form/?',
     'live_esewa_url' => 'https://esewa.com.np/epay/main/?',
 ];
